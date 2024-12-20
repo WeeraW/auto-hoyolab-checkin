54ec95360d398bf0f38772369f9ed5733a63626c
Weerapong Wisomka <teendoi.sp@gmail.com>
2024-12-20 11:14:01 +0700

    Add cookie name constants for session management

--- app/myconsants/constants.go
+++ app/myconsants/constants.go
@@ -4,3 +4,39 @@ const (
 	// AppName is the name of the application
 	AppName = "Automatic Hoyolab Check-in"
 )
+
+var CookieNames = []string{
+	"_HYVUUID",
+	"_MHYUUID",
+	"account_id_v2",
+	"account_mid_v2",
+	"cookie_token_v2",
+	"DEVICEFP_SEED_ID",
+	"DEVICEFP_SEED_TIME",
+	"DEVICEFP",
+	// session cookies
+	// "HYV_LOGIN_PLATFORM_LIFECYCLE_ID",
+	// session cookies
+	// "HYV_LOGIN_PLATFORM_LOAD_TIMEOUT",
+	// session cookies
+	// "HYV_LOGIN_PLATFORM_OPTIONAL_AGREEMENT",
+	// session cookies
+	// "HYV_LOGIN_PLATFORM_TRACKING_MAP",
+	"ltmid_v2",
+	"ltoken_v2",
+	"ltoken",
+	"ltuid_v2",
+	"ltuid",
+	"mi18nLang",
+}
+
+var CookieNamesForZZZ = []string{
+	// session cookies
+	"HYV_LOGIN_PLATFORM_LIFECYCLE_ID",
+	// session cookies
+	"HYV_LOGIN_PLATFORM_LOAD_TIMEOUT",
+	// session cookies
+	"HYV_LOGIN_PLATFORM_OPTIONAL_AGREEMENT",
+	// session cookies
+	"HYV_LOGIN_PLATFORM_TRACKING_MAP",
+}
